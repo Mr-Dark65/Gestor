d6ab156ac8191b4b0bd3e783b0f1da1e1ebb8d32
ACHEDARK <146405764+ACHEDARK@users.noreply.github.com>
2024-03-13 16:14:48 -0500

    visualizador
    Visualizador de Reporte

--- GestorCalificaciones/src/Principal/Main.java
+++ GestorCalificaciones/src/Principal/Main.java
@@ -9,5 +9,29 @@ package Principal;
  * @author thexe
  */
 public class Main {
-    
+
+class ReporteCalificaciones {
+    private List<Calificacion> calificaciones;
+
+    public ReporteCalificaciones(List<Calificacion> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+
+    public void mostrarCalificaciones(String materiaSeleccionada) {
+        // Imprimir encabezados de la tabla
+        System.out.println("+--------------+--------------+--------------+--------------+");
+        System.out.println("|   Materia    |    Fecha     |   Nombre     | Calificación |");
+        System.out.println("+--------------+--------------+--------------+--------------+");
+
+        // Imprimir las calificaciones para la materia seleccionada
+        for (Calificacion calificacion : calificaciones) {
+            if (calificacion.getMateria().equals(materiaSeleccionada)) {
+                System.out.printf("| %-12s | %-12s | %-12s | %-12d |\n", calificacion.getMateria(), calificacion.getFecha(), calificacion.getNombre(), calificacion.getCalificacion());
+            }
+        }
+
+        // Imprimir línea final de la tabla
+        System.out.println("+--------------+--------------+--------------+--------------+\n");
+    }
+}
 }
